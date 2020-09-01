deca57b73a1f497234ebf2eaac9200280930b586
“SM” <samanthaqgm@gmail.com>
2020-09-01 16:01:03 -0400

    added styling

--- world-cup-2018/app.js
+++ world-cup-2018/app.js
@@ -3,20 +3,35 @@ $(() => {
   $('.button').on('click', (event) => {
     event.preventDefault();
 
-    // VARIABLE FOR COUNTRY/SOURCE
-    let $source = $(event.target).attr('id')
-    console.log($source);
-
-    // LINK BASED ON SOURCE:
-    let link = `https://cors-anywhere.herokuapp.com/http://newsapi.org/v2/top-headlines?country=${$source}&apiKey=e3e7c45e43584108a7233aca72bf5f4c`
-
     $.ajax({
       headers: { 'X-Auth-Token': 'fbf56603e5ad42e1ad5448b7430a694a'},
       url: "https://cors-anywhere.herokuapp.com/https://api.football-data.org/v2/competitions/WC/matches?season=2018",
       dataType: 'json',
       type: "GET",
     }).done(datas => {
-        console.log(datas);
+      const $matches = datas.matches
+
+      // For Of Loop iterates over all matches
+      for (let match of $matches) {
+
+        // Variables for team names:
+        const homeTeamName = match.homeTeam.name
+        const awayTeamName = match.awayTeam.name
+
+        // Variables for scores:
+        const homeTeamScore = match.score.fullTime.homeTeam
+        const awayTeamScore = match.score.fullTime.awayTeam
+
+        // Variable for stage (Group, Round of 16, Quarter-finals, etc):
+        const matchStage = match.group
+
+        console.log(match);
+        // console.log(matchStage);
+
+        // console.log(`${matchStage}: ${homeTeamName} ${homeTeamScore} - ${awayTeamScore}  ${awayTeamName}`);
+
+      }
+
     })
 
   })
