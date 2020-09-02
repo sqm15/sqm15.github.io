38b40ce6d7c96a47acef136ac124b3541734ac86
“SM” <samanthaqgm@gmail.com>
2020-09-01 21:39:40 -0400

    added initial functionality

--- world-cup-2018/app.js
+++ world-cup-2018/app.js
@@ -1,18 +1,37 @@
 $(() => {
 
+  const openModal = (displayString) => {
+    $('.modal-text').text(displayString)
+    $('.modal').css('display', 'block')
+  }
+
+  const closeModal = () => {
+    $('.modal').css('display', 'none')
+  }
+
+  $('#close-btn').on('click', () => {
+    event.preventDefault();
+    closeModal();
+  })
+
   $('.button').on('click', (event) => {
     event.preventDefault();
 
+    const $buttonDate = $(event.target).attr('id')
+
+    const link = `https://cors-anywhere.herokuapp.com/https://api.football-data.org/v2/competitions/WC/matches`
+
     $.ajax({
       headers: { 'X-Auth-Token': 'fbf56603e5ad42e1ad5448b7430a694a'},
-      url: "https://cors-anywhere.herokuapp.com/https://api.football-data.org/v2/competitions/WC/matches?season=2018",
+      url: link,
       dataType: 'json',
       type: "GET",
     }).done(datas => {
-      const $matches = datas.matches
+      const matches = datas.matches
+      let outputString = ""
 
       // For Of Loop iterates over all matches
-      for (let match of $matches) {
+      for (let match of matches) {
 
         // Variables for team names:
         const homeTeamName = match.homeTeam.name
@@ -25,12 +44,19 @@ $(() => {
         // Variable for stage (Group, Round of 16, Quarter-finals, etc):
         const matchStage = match.group
 
-        console.log(match);
-        // console.log(matchStage);
+        // Variable for date
+        const matchDate = match.utcDate
 
-        // console.log(`${matchStage}: ${homeTeamName} ${homeTeamScore} - ${awayTeamScore}  ${awayTeamName}`);
-
+        // Checks API dates against button IDs
+        if (matchDate.startsWith($buttonDate)) {
+          // Generates output string with data
+          outputString = outputString + "\n\n" + `${matchStage}:` +
+           "\n" + `${homeTeamName} ${homeTeamScore} - ${awayTeamScore} ${awayTeamName}`
+        }
       }
+
+      // Invoke function:
+      openModal(outputString)
 
     })
 
